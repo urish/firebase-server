e8b899828cce7cdc31c202d04034c7079d7ba184
Uri Shaked <uri@salsa4fun.co.il>
2014-09-06 03:40:30 +0300

    Switch indent to tabs

--- index.js
+++ index.js
@@ -4,69 +4,69 @@ var WebSocketServer = require('ws').Server;
 var mockfirebase = require('mockfirebase');
 
 function FirebaseServer(port, name, data) {
-    this.name = name || 'mock.firebase.server';
-    this.mockFb = new mockfirebase.MockFirebase('https://' + name + '/', data);
+	this.name = name || 'mock.firebase.server';
+	this.mockFb = new mockfirebase.MockFirebase('https://' + name + '/', data);
 
-    var wss = new WebSocketServer({
-        port: port
-    });
+	var wss = new WebSocketServer({
+		port: port
+	});
 
-    wss.on('connection', this.handleConnection.bind(this));
+	wss.on('connection', this.handleConnection.bind(this));
 }
 
 FirebaseServer.prototype = {
-    handleConnection: function (ws) {
-        console.log('new connection');
+	handleConnection: function (ws) {
+		console.log('new connection');
 
-        function send(message) {
-            console.log('sending', message);
-            ws.send(JSON.stringify(message));
-        }
+		function send(message) {
+			console.log('sending', message);
+			ws.send(JSON.stringify(message));
+		}
 
-        ws.on('message', function (data) {
-            console.log('client', data);
-            if (data === 0) {
-                return;
-            }
-            var parsed = JSON.parse(data);
-            if (parsed.t === 'd') {
-                var path = parsed.d.b.p.substr(1);
-                var requestId = parsed.d.r;
+		ws.on('message', function (data) {
+			console.log('client', data);
+			if (data === 0) {
+				return;
+			}
+			var parsed = JSON.parse(data);
+			if (parsed.t === 'd') {
+				var path = parsed.d.b.p.substr(1);
+				var requestId = parsed.d.r;
 				var fbRef = path ? this.mockFb.child(path) : this.mockFb;
-                if (parsed.d.a === 'l') {
-                    console.log('listen', path);
-                    // listen
-                    send({d: {r: requestId, b: {s: 'ok', d: ''}}, t: 'd'});
+				if (parsed.d.a === 'l') {
+					console.log('listen', path);
+					// listen
+					send({d: {r: requestId, b: {s: 'ok', d: ''}}, t: 'd'});
 					fbRef.on('value', function (snap) {
-                        if (snap.val()) {
-                            send({d: {a: 'd', b: {p: path, d: snap.val(), t: null}}, t: 'd'});
-                        }
-                    });
-                    this.mockFb.flush();
-                }
-                if (parsed.d.a === 'p') {
-                    console.log('update', path);
+						if (snap.val()) {
+							send({d: {a: 'd', b: {p: path, d: snap.val(), t: null}}, t: 'd'});
+						}
+					});
+					this.mockFb.flush();
+				}
+				if (parsed.d.a === 'p') {
+					console.log('update', path);
 					fbRef.set(parsed.d.b.d, function () {
 						// TODO check for failure
-                        send({d: {r: requestId, b: {s: 'ok', d: ''}}, t: 'd'});
-                    });
-                }
-            }
-        }.bind(this));
+						send({d: {r: requestId, b: {s: 'ok', d: ''}}, t: 'd'});
+					});
+				}
+			}
+		}.bind(this));
 
-        send({
-            d: {
-                t: 'h',
-                d: {
-                    ts: new Date().getTime(),
-                    v: '5',
-                    h: this.name,
-                    s: ''
-                }
-            },
-            t: 'c'
-        });
-    }
+		send({
+			d: {
+				t: 'h',
+				d: {
+					ts: new Date().getTime(),
+					v: '5',
+					h: this.name,
+					s: ''
+				}
+			},
+			t: 'c'
+		});
+	}
 };
 
 module.exports = FirebaseServer;
