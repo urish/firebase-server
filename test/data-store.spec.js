a6daf9231c39f6b0e5bb4c3a5e66232ba10c873a
James Talmage <james@talmage.io>
2015-11-01 02:26:05 -0500

    drop promisified functions in favor of fireproof

--- test/data-store.spec.js
+++ test/data-store.spec.js
@@ -16,42 +16,26 @@ describe('data-store', () => {
 
 	beforeEach(() => store = new DataStore());
 
-	it('snap() should return a snap asynchronously', function *() {
+	it('(yield ref()).val() should return snap.val() ', function *() {
 		store.ref().set({foo: 'bar'});
 		assert.deepEqual(
-			(yield store.snap()).val(),
+			(yield store.ref()).val(),
 			{foo: 'bar'}
 		);
 	});
 
-	it('snapSync() should return a snap synchronously', () => {
+	it('sync().val() should return snap.val() synchronously', () => {
 		store.ref().set({foo: 'bar'});
 		assert.deepEqual(
-			store.snapSync().val(),
+			store.sync().val(),
 			{foo: 'bar'}
 		);
 	});
 
-	it('val() should return snap.val() asynchronously', function *() {
-		store.ref().set({foo: 'bar'});
-		assert.deepEqual(
-			yield store.val(),
-			{foo: 'bar'}
-		);
-	});
-
-	it('valSync() should return snap.val() synchronously', () => {
-		store.ref().set({foo: 'bar'});
-		assert.deepEqual(
-			store.valSync(),
-			{foo: 'bar'}
-		);
-	});
-
-	it('exportVal() should return snap.exportVal() asynchronously', function *() {
+	it('(yield ref()).exportVal() should return snap.exportVal()', function *() {
 		store.ref('foo').setWithPriority('bar', 3);
 		assert.deepEqual(
-			yield store.exportVal(),
+			(yield store.ref()).exportVal(),
 			{
 				foo: {
 					'.value': 'bar',
@@ -61,10 +45,10 @@ describe('data-store', () => {
 		);
 	});
 
-	it('exportValSync() should return snap.exportVal() synchronously', () => {
+	it('sync().exportVal() should return snap.exportVal() synchronously', () => {
 		store.ref('foo').setWithPriority('bar', 3);
 		assert.deepEqual(
-			store.exportValSync(),
+			store.sync().exportVal(),
 			{
 				foo: {
 					'.value': 'bar',
@@ -72,60 +56,5 @@ describe('data-store', () => {
 				}
 			}
 		);
-	});
-
-	it('getPriority() should return snap.getPriority() asynchronously', function *() {
-		store.ref('foo').setWithPriority('bar', 3);
-		assert.strictEqual(
-			yield store.getPriority('foo'),
-			3
-		);
-	});
-
-	it('exists() / existsSync() should be proxied', function *() {
-		store.ref().set({foo: 'bar'});
-		assert.strictEqual(
-			yield store.exists('foo'),
-			true
-		);
-		assert.strictEqual(
-			store.existsSync('baz'),
-			false
-		);
-	});
-
-	it('hasChildren() / hasChildrenSync() should be proxied', function *() {
-		store.ref().set({foo: {bar: 'baz'}, quz: 3});
-		assert.strictEqual(
-			yield store.hasChildren('foo'),
-			true
-		);
-		assert.strictEqual(
-			store.hasChildrenSync('quz'),
-			false
-		);
-	});
-
-	it('data access methods (snap, snapSync, val, ...etc) should accept a path parameter', function *() {
-		store.ref('foo/bar').setWithPriority('baz', 5);
-		store.ref('foo/quz').setWithPriority('foo', 6);
-		assert.deepEqual(
-			(yield store.snap('foo/bar')).exportVal(),
-			{
-				'.value': 'baz',
-				'.priority': 5
-			}
-		);
-
-		assert.deepEqual(
-			store.snapSync('foo/bar').exportVal(),
-			{
-				'.value': 'baz',
-				'.priority': 5
-			}
-		);
-
-		assert.strictEqual(yield store.val('foo/quz'), 'foo');
-		assert.strictEqual(store.valSync('foo/quz'), 'foo');
 	});
 });
