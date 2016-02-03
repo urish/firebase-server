6f8d3b885fa812c60239dccea0bec174925e99bd
Uri Shaked <uri@salsa4fun.co.il>
2016-02-03 21:49:34 +0200

    feat(cli): add a cli tool for invoking firebase server

---
+++ bin/firebase-server.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var cli = require('cli');
+var debug = require('debug');
+
+cli.parse({
+	verbose: ['v', 'Enable verbose (debug) output'],
+	port: ['p', 'Listen on this port', 'number', 5000],
+	name: ['n', 'Hostname of the firebase server', 'name', 'localhost.firebaseio.test']
+});
+
+cli.main(function (args, options) {
+	if (options.verbose) {
+		debug.enable('firebase-server*');
+	}
+
+	var FirebaseServer = require('../index.js');
+
+	new FirebaseServer(options.port, options.name, {}); // eslint-disable-line no-new
+
+	this.ok('Listening on port ' + options.port);
+});
+
+
