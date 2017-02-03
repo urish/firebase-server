4e8440a28c5decc899260f6f92d9e1ad16980da7
Maik Vlcek <maik@mediavrog.net>
2017-02-03 16:43:30 +0900

    fixes #77 add CLI parameter to load file or json

--- bin/firebase-server.js
+++ bin/firebase-server.js
@@ -2,13 +2,15 @@
 
 'use strict';
 
+var fs = require('fs');
 var cli = require('cli');
 var debug = require('debug');
 
 cli.parse({
 	verbose: ['v', 'Enable verbose (debug) output'],
 	port: ['p', 'Listen on this port', 'number', 5000],
-	name: ['n', 'Hostname of the firebase server', 'string', 'localhost.firebaseio.test']
+	name: ['n', 'Hostname of the firebase server', 'string', 'localhost.firebaseio.test'],
+	data: ['d', 'JSON String or File with JSON data to bootstrap the server with', 'string', '{}']
 });
 
 cli.main(function (args, options) {
@@ -18,7 +20,19 @@ cli.main(function (args, options) {
 
 	var FirebaseServer = require('../index.js');
 
-	new FirebaseServer(options.port, options.name, {}); // eslint-disable-line no-new
+	var data = {};
+	try {
+		fs.statSync(options.data); // eslint-disable-line no-sync
+		data = require(options.data);
+	} catch (eFile) {
+		try {
+			data = JSON.parse(options.data);
+		} catch (eJson) {
+			data = {};
+		}
+	}
+
+	new FirebaseServer(options.port, options.name, data); // eslint-disable-line no-new
 
 	this.ok('Listening on port ' + options.port);
 });
