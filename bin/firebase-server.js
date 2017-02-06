f082e240e099e9e570ac416ddc7f86f734d53228
Maik Vlcek <maik@mediavrog.net>
2017-02-06 18:03:03 +0900

    fail on error

--- bin/firebase-server.js
+++ bin/firebase-server.js
@@ -11,8 +11,8 @@ cli.parse({
 	verbose: ['v', 'Enable verbose (debug) output'],
 	port: ['p', 'Listen on this port', 'number', 5000],
 	name: ['n', 'Hostname of the firebase server', 'string', 'localhost.firebaseio.test'],
-	data: ['d', 'JSON String data to bootstrap the server with', 'string', '{}'],
-	file: ['f', 'JSON File to bootstrap the server with', 'file']
+	data: ['d', 'JSON data to bootstrap the server with', 'string', '{}'],
+	file: ['f', 'JSON file to bootstrap the server with', 'file']
 });
 
 cli.main(function (args, options) {
@@ -21,12 +21,25 @@ cli.main(function (args, options) {
 	}
 
 	var FirebaseServer = require('../index.js');
-	var data = {};
 
+	var rawData;
+	if (options.file) {
+		try {
+			rawData = fs.readFileSync(path.resolve(process.cwd(), options.file)); // eslint-disable-line no-sync
+		} catch (e) {
+			this.output(e);
+			this.fatal('Provided file could not be read.');
+		}
+	} else {
+		rawData = options.data;
+	}
+
+	var data = {};
 	try {
-		data = JSON.parse(options.file || options.data);
+		data = JSON.parse(rawData);
 	} catch (e) {
-		console.warn('Provided content was not valid JSON');
+		this.output(e);
+		this.fatal('Provided data was not valid JSON.');
 	}
 
 	new FirebaseServer(options.port, options.name, data); // eslint-disable-line no-new
