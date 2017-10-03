6116ac89a157701fd32393117dfb0566fe075cc7
Oleg Pudeyev <oleg@bsdpower.com>
2017-10-03 11:51:15 -0400

    eslint fix again

--- index.js
+++ index.js
@@ -58,76 +58,76 @@ function normalizePath(fullPath) {
 }
 
 function HttpServer(port, db) {
-  			function read(path, cb) {
-	db.ref(path).once('value').then(cb);
-  }
+	function read(path, cb) {
+					db.ref(path).once('value').then(cb);
+				}
 
-  			function writeResponse(response, payload) {
-	response.writeHead(200, {'Content-Type': 'application/json'});
-	response.write(JSON.stringify(payload));
-	response.end();
-  }
-
-  			function handleReadRequest(request, response, path) {
-	read(path, function(snapshot) {
-	  					writeResponse(response, snapshot.val());
-			});
-  }
-
-  			function handleWriteRequest(request, response, path, writeMethod) {
-	var body = '';
-	request.on('data', function(data) {
-	  					body += data;
-	  					if (body.length > 1e6)
-		request.connection.destroy();
-			});
-	request.on('end', function () {
-	  					var payload = JSON.parse(body);
-	  					db.ref(path)[writeMethod](payload);
-	  					if (writeMethod === 'update') {
-		read(path, function(snapshot) { writeResponse(response, snapshot.val()); });
-	  } else {
-		writeResponse(response, payload);
-	  }
-			});
-  }
-
-  			function handleDeleteRequest(request, response, path) {
-	db.ref(path).remove();
-	writeResponse(response, null);
-  }
-
-  			var server = http.createServer(function(request, response) {
-	var urlParts = url.parse(request.url);
-	var path = urlParts.pathname;
-	if (!path.match(/\.json$/)) {
-	  					response.writeHead(404);
-	  					response.end();
-	  					return;
-			}
-	path = path.replace(/\.json$/, '');
-	console.log(request.method + ' ' + path);
-	switch (request.method) {
-	  					case 'GET':
-					handleReadRequest(request, response, path);
-					break;
-	  					case 'PUT':
-					handleWriteRequest(request,response,  path, 'set');
-					break;
-	  					case 'PATCH':
-					handleWriteRequest(request, response, path, 'update');
-					break;
-	  					case 'DELETE':
-					handleDeleteRequest(request, response, path);
-					break;
-	  					default:
-					response.writeHead(400);
+	function writeResponse(response, payload) {
+					response.writeHead(200, {'Content-Type': 'application/json'});
+					response.write(JSON.stringify(payload));
 					response.end();
-			}
-  });
+				}
 
-  			server.listen(port, '0.0.0.0');
-  			return server;
+	function handleReadRequest(request, response, path) {
+					read(path, function(snapshot) {
+		writeResponse(response, snapshot.val());
+	});
+				}
+
+	function handleWriteRequest(request, response, path, writeMethod) {
+					var body = '';
+					request.on('data', function(data) {
+		body += data;
+		if (body.length > 1e6)
+								request.connection.destroy();
+	});
+					request.on('end', function () {
+		var payload = JSON.parse(body);
+		db.ref(path)[writeMethod](payload);
+		if (writeMethod === 'update') {
+								read(path, function(snapshot) { writeResponse(response, snapshot.val()); });
+							} else {
+								writeResponse(response, payload);
+							}
+	});
+				}
+
+	function handleDeleteRequest(request, response, path) {
+					db.ref(path).remove();
+					writeResponse(response, null);
+				}
+
+	var server = http.createServer(function(request, response) {
+					var urlParts = url.parse(request.url);
+					var path = urlParts.pathname;
+					if (!path.match(/\.json$/)) {
+		response.writeHead(404);
+		response.end();
+		return;
+	}
+					path = path.replace(/\.json$/, '');
+					console.log(request.method + ' ' + path);
+					switch (request.method) {
+		case 'GET':
+			handleReadRequest(request, response, path);
+			break;
+		case 'PUT':
+			handleWriteRequest(request,response,	path, 'set');
+			break;
+		case 'PATCH':
+			handleWriteRequest(request, response, path, 'update');
+			break;
+		case 'DELETE':
+			handleDeleteRequest(request, response, path);
+			break;
+		default:
+			response.writeHead(400);
+			response.end();
+	}
+				});
+
+	server.listen(port, '0.0.0.0');
+	return server;
 }
 
 function FirebaseServer(port, name, data) {
