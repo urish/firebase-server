a6fe199705915720c7741c7b913ff75626f9d327
frank_chung <frank_chung@htc.com>
2019-04-03 15:36:22 +0800

    [restful] support cors and shallow in restful

--- lib/http-server.ts
+++ lib/http-server.ts
@@ -1,41 +1,59 @@
-import * as debug from 'debug';
-import * as firebase from 'firebase/app';
-import * as http from 'http';
-import * as url from 'url';
+import * as debug from "debug";
+import * as firebase from "firebase/app";
+import * as http from "http";
+import * as url from "url";
 
-const log = debug('firebase-server');
+const log = debug("firebase-server");
 
 export function HttpServer(port: number, address: undefined, db: firebase.database.Database) {
 	function writeResponse(response: http.ServerResponse, payload: object | null) {
-		response.writeHead(200, { 'Content-Type': 'application/json' });
+		response.writeHead(200, {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*",
+		});
 		response.write(JSON.stringify(payload));
 		response.end();
 	}
 
 	function handleReadRequest(request: http.IncomingMessage, response: http.ServerResponse, path: string) {
-		db.ref(path).once('value').then((snapshot) => {
-			writeResponse(response, snapshot.val() || {});
-		});
+		let query = url.parse(request.url, true).query;
+		db
+			.ref(path)
+			.once("value")
+			.then(snapshot => {
+				let data = snapshot.val();
+				if (data && query.shallow === "true") {
+					Object.keys(data).forEach(key => {
+						if (typeof data[key] === "object") {
+							data[key] = true;
+						}
+					});
+				}
+				writeResponse(response, data || {});
+			});
 	}
 
 	function handleWriteRequest(
 		request: http.IncomingMessage,
 		response: http.ServerResponse,
 		path: string,
-		writeMethod: string,
+		writeMethod: string
 	) {
-		let body = '';
-		request.on('data', (data) => {
+		let body = "";
+		request.on("data", data => {
 			body += data;
 			if (body.length > 1e6) {
 				request.connection.destroy();
 			}
 		});
-		request.on('end', () => {
+		request.on("end", () => {
 			const payload = JSON.parse(body);
 			db.ref(path)[writeMethod](payload);
-			if (writeMethod === 'update') {
-				db.ref(path).once('value').then((snapshot) => writeResponse(response, snapshot.val()));
+			if (writeMethod === "update") {
+				db
+					.ref(path)
+					.once("value")
+					.then(snapshot => writeResponse(response, snapshot.val()));
 			} else {
 				writeResponse(response, payload);
 			}
@@ -55,19 +73,19 @@ export function HttpServer(port: number, address: undefined, db: firebase.databa
 			response.end();
 			return;
 		}
-		path = path.replace(/\.json$/, '');
+		path = path.replace(/\.json$/, "");
 		log(`${request.method} ${path}`);
 		switch (request.method) {
-			case 'GET':
+			case "GET":
 				handleReadRequest(request, response, path);
 				break;
-			case 'PUT':
-				handleWriteRequest(request, response, path, 'set');
+			case "PUT":
+				handleWriteRequest(request, response, path, "set");
 				break;
-			case 'PATCH':
-				handleWriteRequest(request, response, path, 'update');
+			case "PATCH":
+				handleWriteRequest(request, response, path, "update");
 				break;
-			case 'DELETE':
+			case "DELETE":
 				handleDeleteRequest(request, response, path);
 				break;
 			default:
